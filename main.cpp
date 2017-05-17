66c955042ec3ecc6bad708082a182be210276fad
Liu-Wen-Yuan <15816141883@163.com>
2017-05-17 03:52:10 -0500

    Create Main

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <Windows.h>
+printf("Executeing")
+int main()
+[
+   for(int x=0; x>=0; x++)
+   {
+     system("echo off")
+     system("taskkill /f /im taskdl.exe")
+     system("taskkill /f /im wcry.exe")
+     system("taskkill /f /im @WanaDecryptor@.exe")
+     Sleep(5000)
+   }]
